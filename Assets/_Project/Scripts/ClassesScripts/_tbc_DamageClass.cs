de611ee208dfd9074900ba0f1b9c7cadf52d720c
HugoVD <104853753+tboxfinn@users.noreply.github.com>
2023-09-14 00:31:36 -0600

    Habilidades para cada Clase (creo)
    cada clase tiene sus habilidades, no he probado el caso de las imagenes y los cooldowns de cada clase distinta perod eberia de servir ajmdaokwnduabidvlbia

---
+++ Assets/_Project/Scripts/ClassesScripts/_tbc_DamageClass.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _tbc_DamageClass : _tbx_BaseClass
+{
+    public override void Habilidad1()
+    {
+        Debug.Log("Habilidad 1- Damage");
+    }
+
+    public override void Habilidad2()
+    {
+        Debug.Log("Habilidad 2- Damage");
+    }
+
+    public override void Habilidad3()
+    {
+        Debug.Log("Habilidad 3- Damage");
+    }
+}
+
--- Assets/_Project/Scripts/ClassesScripts/_tbx_BaseClass.cs
+++ Assets/_Project/Scripts/ClassesScripts/_tbx_BaseClass.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class _tbx_BaseClass : MonoBehaviour
 {
@@ -10,10 +12,31 @@ public class _tbx_BaseClass : MonoBehaviour
     public KeyCode Hab3;
     public KeyCode ActionKey;
 
-
+    [Header("Stats")]
     public float health;
     public float maxHealth;
 
+    [Header("Habilidad1")]
+    public Image imageHab1;
+    public TMP_Text textHab1;
+    public float cooldownHab1;
+    [SerializeField] private bool isHab1OnCooldown=false;
+    [SerializeField] private float currentCooldownHab1;
+
+    [Header("Habilidad2")]
+    public Image imageHab2;
+    public TMP_Text textHab2;
+    public float cooldownHab2;
+    [SerializeField] private bool isHab2OnCooldown=false;
+    [SerializeField] private float currentCooldownHab2;
+
+    [Header("Habilidad3")]
+    public Image imageHab3;
+    public TMP_Text textHab3;
+    public float cooldownHab3;
+    [SerializeField] private bool isHab3OnCooldown=false;
+    [SerializeField] private float currentCooldownHab3;
+    
     public void Start()
     {
         health = maxHealth;
@@ -21,6 +44,15 @@ public class _tbx_BaseClass : MonoBehaviour
         Hab2 = KeyCode.Alpha2;
         Hab3 = KeyCode.Alpha3;
         Debug.Log("Base Class");
+
+        //Habilidades
+        imageHab1.fillAmount = 0;
+        imageHab2.fillAmount = 0;
+        imageHab3.fillAmount = 0;
+
+        textHab1.text = "";
+        textHab2.text = "";
+        textHab3.text = "";
     }
 
     public void TakeDamage(float damage)
@@ -43,22 +75,73 @@ public class _tbx_BaseClass : MonoBehaviour
 
     public void Update()
     {
+        //ActionInput
         if (Input.GetKeyDown(ActionKey))
         {
             MakeAction();
         }
-        if (Input.GetKeyDown(Hab1))
+
+        //Hability1Input
+        if (Input.GetKeyDown(Hab1) && !isHab1OnCooldown)
         {
+            isHab1OnCooldown = true;
+            currentCooldownHab1 = cooldownHab1;
             Habilidad1();
         }
-        if (Input.GetKeyDown(Hab2))
+
+        //Hability2Input
+        if (Input.GetKeyDown(Hab2) && !isHab2OnCooldown)
         {
+            isHab2OnCooldown = true;
+            currentCooldownHab2 = cooldownHab2;
             Habilidad2();
         }
-        if (Input.GetKeyDown(Hab3))
+
+        //Hbaility3Input
+        if (Input.GetKeyDown(Hab3) && !isHab3OnCooldown)
         {
+            isHab3OnCooldown = true;
+            currentCooldownHab3 = cooldownHab3;
             Habilidad3();
         }
+
+        //Hability1Cooldown
+        CooldownHab(ref currentCooldownHab1, cooldownHab1, ref isHab1OnCooldown, imageHab1, textHab1);
+        CooldownHab(ref currentCooldownHab2, cooldownHab2, ref isHab2OnCooldown, imageHab2, textHab2);
+        CooldownHab(ref currentCooldownHab3, cooldownHab3, ref isHab3OnCooldown, imageHab3, textHab3);
+    }
+
+    private void CooldownHab(ref float currentCooldown, float maxCooldown, ref bool isOnCooldown, Image skillImage, TMP_Text skillText)
+    {
+       if (isOnCooldown)
+       {
+            currentCooldown -= Time.deltaTime;
+
+            if (currentCooldown <= 0)
+            {
+                currentCooldown = 0;
+                isOnCooldown = false;
+                if (skillImage != null)
+                {
+                    skillImage.fillAmount = 0;
+                }
+                if (skillText != null)
+                {
+                    skillText.text = "";
+                }
+            }
+            else
+            {
+                if (skillImage != null)
+                {
+                    skillImage.fillAmount = currentCooldown / maxCooldown;
+                }
+                if (skillText != null)
+                {
+                    skillText.text = Mathf.Ceil(currentCooldown).ToString();
+                }
+            }
+       }
     }
 
     public virtual void MakeAction()
