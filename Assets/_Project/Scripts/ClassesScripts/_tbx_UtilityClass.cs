16b2b995dacaafda6c13c1e975bdefa5869905f4
HugoVD <104853753+tboxfinn@users.noreply.github.com>
2023-09-13 21:00:57 -0600

    Added Clase Utility

--- Assets/_Project/Scripts/ClassesScripts/_tbx_UtilityClass.cs
+++ Assets/_Project/Scripts/ClassesScripts/_tbx_UtilityClass.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class _tbx_SupportClass : _tbx_BaseClass
+public class _tbx_UtilityClass : _tbx_BaseClass
 {
     public override void Habilidad1()
     {
-        Debug.Log("Habilidad 1- Support");
+        Debug.Log("Habilidad 1- Utility");
     }
 
     public override void Habilidad2()
     {
-        Debug.Log("Habilidad 2- Support");
+        Debug.Log("Habilidad 2- Utility");
     }
 
     public override void Habilidad3()
     {
-        Debug.Log("Habilidad 3- Support");
+        Debug.Log("Habilidad 3- Utility");
     }
 }
