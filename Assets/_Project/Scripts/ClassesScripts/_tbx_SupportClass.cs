a6ddcae5440e46c859772da654905b2f2ecf6731
PortlyMoney4024 <104855108+PortlyMoney4024@users.noreply.github.com>
2023-10-24 11:34:23 -0600

    update habilidad 2 support
    ya casi funciona, checar porque no cambia la vida

--- Assets/_Project/Scripts/ClassesScripts/_tbx_SupportClass.cs
+++ Assets/_Project/Scripts/ClassesScripts/_tbx_SupportClass.cs
@@ -10,6 +10,15 @@ public class _tbx_SupportClass : _tbx_BaseClass
 
     [SerializeField] private float initialMoveSpeed;
 
+    public float fuerzaDeTiro;
+    public float distanciaSpawnHabilidad1;
+    public List<GameObject> habilidad1Objects = new List<GameObject>();
+    public GameObject prefabHabilidad1;
+    public int cantidadMaxHabilidad1;
+    private int cantidadHabilidad1Actual = 0;
+    public GameObject player;
+    public GameObject playerObj;
+
     public override void Start()
     {
         if (_ply_PlayerHealth.instance != null)
@@ -43,6 +52,39 @@ public class _tbx_SupportClass : _tbx_BaseClass
     public override void Habilidad2()
     {
         Debug.Log("Habilidad 2- Support");
+        if (cantidadHabilidad1Actual < cantidadMaxHabilidad1)
+        {
+            // Spawn a new ability object in front of the player
+            GameObject newHabilidad1Object = Instantiate(prefabHabilidad1, playerObj.transform.position + playerObj.transform.forward * distanciaSpawnHabilidad1, Quaternion.identity);
+            Rigidbody newHabilidad1ObjectRigidbody = newHabilidad1Object.GetComponent<Rigidbody>();
+
+            // Apply force to the ability object
+            newHabilidad1ObjectRigidbody.AddForce(playerObj.transform.forward * fuerzaDeTiro, ForceMode.Impulse);
+
+            // Add the new object to the list of spawned objects
+            habilidad1Objects.Add(newHabilidad1Object);
+
+            // Increment the current number of ability objects
+            cantidadHabilidad1Actual++;
+            Destroy(newHabilidad1Object, 5f);
+        }
+        else
+        {
+            // Destroy the first spawned object of this type and remove it from the list
+            GameObject firstHabilidad1Object = habilidad1Objects[0];
+            habilidad1Objects.RemoveAt(0);
+            Destroy(firstHabilidad1Object);
+
+            // Spawn a new ability object in front of the player
+            GameObject newHabilidad1Object = Instantiate(prefabHabilidad1, playerObj.transform.position + playerObj.transform.forward * distanciaSpawnHabilidad1, Quaternion.identity);
+            Rigidbody newHabilidad1ObjectRigidbody = newHabilidad1Object.GetComponent<Rigidbody>();
+
+            // Apply force to the ability object
+            newHabilidad1ObjectRigidbody.AddForce(playerObj.transform.forward * fuerzaDeTiro, ForceMode.Impulse);
+
+            // Add the new object to the list of spawned objects
+            habilidad1Objects.Add(newHabilidad1Object);
+        }
     }
 
     public override void Habilidad3()
