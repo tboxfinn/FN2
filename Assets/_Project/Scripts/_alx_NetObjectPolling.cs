cce606788887d2e377d5057bf6fd5fc9e997c181
Carlos Alejandro <alexco_99@hotmail.com>
2023-10-24 20:43:12 -0600

    Update _alx_NetObjectPolling.cs

--- Assets/_Project/Scripts/_alx_NetObjectPolling.cs
+++ Assets/_Project/Scripts/_alx_NetObjectPolling.cs
@@ -1,30 +1,38 @@
-using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class _alx_NetObjectPolling : NetworkBehaviour
+public class NetworkObjectPooling : NetworkBehaviour
 {
     [SerializeField] private GameObject prefab; // El prefab de la bala que quieres agrupar.
     [SerializeField] private int size; // El tamaño inicial del grupo.
 
     private Queue<NetworkObject> pool; // La cola que contiene las balas agrupadas.
+    private bool isInitialized = false;
 
     // Este método se llama al inicio del juego.
     void Start()
     {
-        if (IsServer)
+        if (IsServer && !isInitialized)
         {
-            pool = new Queue<NetworkObject>(); // Inicializa la cola.
-
-            // Crea las balas y las añade a la cola.
-            for (int i = 0; i < size; i++)
-            {
-                NetworkObject networkObject = Instantiate(prefab).GetComponent<NetworkObject>();
-                networkObject.gameObject.SetActive(false); // Desactiva la bala.
-                pool.Enqueue(networkObject); // Añade la bala a la cola.
-            }
+            InitializePool();
         }
+    }
+
+    // Este método se utiliza para inicializar el grupo de balas.
+    private void InitializePool()
+    {
+        pool = new Queue<NetworkObject>(); // Inicializa la cola.
+
+        // Crea las balas y las añade a la cola.
+        for (int i = 0; i < size; i++)
+        {
+            NetworkObject networkObject = Instantiate(prefab).GetComponent<NetworkObject>();
+            networkObject.gameObject.SetActive(false); // Desactiva la bala.
+            pool.Enqueue(networkObject); // Añade la bala a la cola.
+        }
+
+        isInitialized = true;
     }
 
     // Este método se utiliza para obtener una bala del grupo.
@@ -32,6 +40,11 @@ public class _alx_NetObjectPolling : NetworkBehaviour
     {
         if (IsServer)
         {
+            if (!isInitialized)
+            {
+                InitializePool();
+            }
+
             if (pool.Count == 0)
             {
                 return null; // Si el grupo está vacío, devuelve null.
