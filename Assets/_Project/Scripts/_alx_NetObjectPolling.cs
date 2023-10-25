ea9c9033f69cd33a5404b69d37d665839642d799
Carlos Alejandro <alexco_99@hotmail.com>
2023-10-24 20:14:54 -0600

    Update _alx_NetObjectPolling.cs

--- Assets/_Project/Scripts/_alx_NetObjectPolling.cs
+++ Assets/_Project/Scripts/_alx_NetObjectPolling.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-
+// Esta es la clase principal que maneja el agrupamiento de objetos de red.
 public class _alx_NetObjectPolling : MonoBehaviour
 {
-    [SerializeField] private GameObject prefab;
-    [SerializeField] private int size;
+    [SerializeField] private GameObject prefab; // El prefab que quieres agrupar.
+    [SerializeField] private int size; // El tamaño inicial del grupo.
 
-    private Queue<NetworkObject> pool;
+    private Queue<NetworkObject> pool; // La cola que contiene los objetos agrupados.
 
+    // Este método se llama al inicio del juego.
     void Start()
     {
-        pool = new Queue<NetworkObject>();
+        pool = new Queue<NetworkObject>(); // Inicializa la cola.
 
+        // Crea los objetos y los añade a la cola.
         for (int i = 0; i < size; i++)
         {
             NetworkObject networkObject = Instantiate(prefab).GetComponent<NetworkObject>();
@@ -23,28 +25,32 @@ public class _alx_NetObjectPolling : MonoBehaviour
         }
     }
 
+    // Este método se utiliza para obtener un objeto del grupo.
     public NetworkObject Get()
     {
         if (pool.Count == 0)
         {
-            return null;
+            return null; // Si el grupo está vacío, devuelve null.
         }
 
-        NetworkObject networkObject = pool.Dequeue();
-        networkObject.gameObject.SetActive(true);
+        NetworkObject networkObject = pool.Dequeue(); // Obtiene el primer objeto de la cola.
+        networkObject.gameObject.SetActive(true); // Activa el objeto.
 
-        return networkObject;
+        return networkObject; // Devuelve el objeto.
     }
 
+    // Este método se utiliza para devolver un objeto al grupo.
     public void Return(NetworkObject networkObject)
     {
-        networkObject.gameObject.SetActive(false);
-        pool.Enqueue(networkObject);
+        networkObject.gameObject.SetActive(false); // Desactiva el objeto.
+        pool.Enqueue(networkObject); // Añade el objeto de nuevo a la cola.
     }
 }
 
+// Esta interfaz se utiliza para personalizar cómo se instancian y destruyen los objetos de red.
 public interface INetworkPrefabInstanceHandler
 {
     NetworkObject Instantiate(ulong ownerClientId, Vector3 position, Quaternion rotation);
     void Destroy(NetworkObject networkObject);
 }
+
