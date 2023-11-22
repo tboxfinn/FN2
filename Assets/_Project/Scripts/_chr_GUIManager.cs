ca5e1cff1a1c6442d1a630e383f518da435ee112
Carlos Mario Hernández León <84859398+CharloHdz@users.noreply.github.com>
2023-11-21 20:35:42 -0600

    Ya se puede modificar el shader con codigo

--- Assets/_Project/Scripts/_chr_GUIManager.cs
+++ Assets/_Project/Scripts/_chr_GUIManager.cs
@@ -11,26 +11,17 @@ using UnityEngine.Timeline;
 
 public class _chr_GUIManager : MonoBehaviour
 {
-    [Header("GUI States")]
-    public GUIState GUIStat;
-    [Space(15)]
-    [Header("Telon")]
-    [SerializeField] PlayableDirector TelonAmim;
-    [Space(15)]
-    [Header ("Menu Panel")]
-    [SerializeField] GameObject MenuPanel;
-    [SerializeField] PlayableDirector StartBtnAnim;
-
-    [Space(15)]
-    [Header("Character Selector Panel")]
-    [SerializeField] GameObject CharacterSelectorPanel;
-
-    [Space(15)]
-    [Header("Other Stuff")]
-    [SerializeField] bool isCoroutineRunning;
-
     [Space (15)]
     [SerializeField] private Material areaMat1, areaMat2, areaMat3;
+
+    [Range (6, 26)]
+    public float LifeArea1;
+
+    [Range (6, 26)]
+    public float LifeArea2;
+
+    [Range (6, 26)]
+    public float LifeArea3;
 
     public void OnEnable()
     {
@@ -44,44 +35,8 @@ public class _chr_GUIManager : MonoBehaviour
 
     void Update()
     {
-        switch(GUIStat){
-            case GUIState.Menu:
-            //TelonAnim.SetTrigger("AnimCompleted");
-            break;
-        }
+        areaMat1.SetFloat("_Cutoff_height", LifeArea1);
+        areaMat2.SetFloat("_Cutoff_height", LifeArea2);
+        areaMat3.SetFloat("_Cutoff_height", LifeArea3);
     }
-
-    public void StartGame(){
-
-        StartBtnAnim.Play();
-            // StartCoroutine(EsperaYRealizaAccion(0.5f, () => {
-            //     start
-            //     CharacterSelectorPanel.SetActive(true);
-            // }));
-    }
-
-
-    //Este IEnumerator permite el poder atrasar las acciones para que las animaciones fluyan
-    private IEnumerator EsperaYRealizaAccion(float tiempoDeEspera, Action accion)
-    {
-        if (isCoroutineRunning)
-        {
-            yield break; // Si ya hay una instancia de la corrutina ejecutándose, no inicies otra.
-        }
-
-        isCoroutineRunning = true;
-
-        yield return new WaitForSeconds(tiempoDeEspera);
-
-        // Llama a la acción después del tiempo de espera.
-        accion.Invoke();
-
-        isCoroutineRunning = false; // Marca la corrutina como finalizada.
-    }
-}
-
-public enum GUIState{
-    Menu,
-    CharacterSelector,
-    Pause
 }
